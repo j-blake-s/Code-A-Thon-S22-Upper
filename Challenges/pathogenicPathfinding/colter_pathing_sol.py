4d85514e95dcc4721ebad270552796a30d3bb70f
Goldenlion5648 <coboudinot@gmail.com>
2022-02-22 23:10:22 -0500

    less broken solution

--- Challenges/pathogenicPathfinding/colter_pathing_sol.py
+++ Challenges/pathogenicPathfinding/colter_pathing_sol.py
@@ -10,67 +10,58 @@ for _ in range(streets):
     dists[from_][to] = min(dist, dists[from_][to])
     dists[to][from_] = min(dist, dists[to][from_])
 
+needed_list = []
+needed_spots = {}
+single_needed = set()
+for q in range(queries):
+    from_, to = input().split()
+    needed_spots[from_] = to
+    needed_spots[to] = from_
+    needed_list.append((from_, to))
 
 
-
-def djikstra(start, goal):
-    DJIK = False
-    if DJIK:
-        fringe = []
-        heapq.heapify(fringe)
-    else:
-        fringe = []
-    # for j, start in enumerate(dists):
-    # print(j)
+DJIK = True
+if DJIK:
+    fringe = []
+    heapq.heapify(fringe)
+else:
+    fringe = deque([])
+for start in dists:
+    if start not in needed_spots:
+        continue
     if DJIK:
         heapq.heappush(fringe, (0, start))
     else:
         fringe.append((0, start))
     # print("start", start)
     seen = set()
-    # cur_graph = dd(lambda : float("inf"))
     while len(fringe):
-        cost, cur = heapq.heappop(fringe) if DJIK else min(fringe)
-        fringe.remove((cost, cur))
-        # print(cur)
+        if DJIK:
+            cost, cur = heapq.heappop(fringe)
+        else:
+            cost, cur = fringe.popleft()
         if cur in seen:
             continue
         seen.add(cur)
-        # if cur == goal:
-            # return cost
         if dists[start][cur] > cost:
             dists[start][cur] = cost
-        # else:
-            # continue
+            dists[cur][start] = cost
 
+        # print(f"{cur} ----")
         for other in dists[cur]:
-            if other in seen:
+            if other == cur:
                 continue
             new = (cost + dists[cur][other], other)
             if DJIK:
-                heapq.heappush(fringe, new)
+                heapq.heappush(fringe, (cost + dists[cur][other], other))
             else:
                 fringe.append(new)
-
-    # dists[start] = cur_graph
-    # print(dists)
-
-    # for n in needed_list:
-        # from_, to = n
-        # print(dists[from_][to])
-
-# needed_list = []
-# needed_spots = {}
-# single_needed = set()
-for _ in range(queries):
-    from_, to = input().split()
-    if to not in dists[from_]:
-        djikstra(from_, to)
+            # print(other)
+        # print(len(fringe))
+for n in needed_list:
+    from_, to = n
     print(dists[from_][to])
-
-    # needed_spots[from_] = to
-    # needed_spots[to] = from_
-    # needed_list.append((from_, to))
+    
 # print(dists)
 # for x in dists:
     # print(x, dists[x])
